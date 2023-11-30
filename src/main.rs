c867fadd8dcc8598a8448b1f9159598951786901
André Ferreira <andre.ferreira@mercatustecnilogia.com.br>
2023-11-30 18:39:34 -0300

    finished restored flow

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,59 @@
-use std::{fs::File, process::Stdio};
+use std::{
+    fs::File,
+    fs::{self},
+    process::Stdio,
+};
+
+const Z_PATH: &str = r"C:\Program Files\7-Zip\7z.exe";
 
 const GBAK: &str = r"C:\Program Files\Firebird\Firebird_3_0\gbak.exe";
 const USER: &str = "SYSDBA";
 const PASSWORD: &str = "masterkey";
-const TEMP_PATH: &str = "C:/app/plus/copia/temp";
-const TO_RESTORE_PATH: &str = r"C:\App\Plus\copia\to_restore";
-const Z_PATH: &str = r"C:\Program Files\7-Zip\7z.exe";
+
+const TEMP_PATH: &str = "C:/Users/dev/restore/temp";
+const TO_RESTORE_PATH: &str = r"C:\Users\dev\restore\to_restore";
+const RESTORED_PATH: &str = "C:/Users/dev/restore/restored";
 
 fn main() {
-    // for file in get_all_compressed_files() {
-    //     let path = file.to_str().unwrap();
-    //     let file = file.file_name().unwrap().to_str().unwrap();
+    for file in get_all_files(&TO_RESTORE_PATH, "7Z") {
+        let file_name = file.file_name().unwrap().to_str().unwrap();
+        let path = file.to_str().unwrap();
 
-    //     println!("File: {} with path {}", file, path);
-    // }
+        unzip(path);
 
-    unzip(r"C:\App\Plus\copia\to_restore\APPDATABASE-TERCA-1.7z");
-    print!("Término do processo");
+        for gbk in get_all_files(&TEMP_PATH, "GBK") {
+            let file_gbk = gbk.file_name().unwrap().to_str().unwrap();
+
+            restore_database(&file_gbk);
+
+            let file_ziped = format!("{}.7z", &file_name);
+            zip(&file_ziped);
+
+            fs::copy(
+                format!("{}/{}", &TEMP_PATH, &file_ziped),
+                format!("{}/{}", &RESTORED_PATH, &file_ziped),
+            )
+            .expect(&format!("Error on copy {}", &file_ziped));
+        }
+
+        fs::remove_dir_all(&TEMP_PATH).expect(&format!("Error on delete dir {}", &TEMP_PATH));
+        fs::create_dir(&TEMP_PATH).expect(&format!("Error on create dir {}", &TEMP_PATH));
+
+        fs::remove_file(&file).unwrap();
+
+        print!("Término do restore {}", file_name);
+    }
 }
 
-fn get_all_compressed_files() -> Vec<std::path::PathBuf> {
+fn get_all_files(path: &str, ext: &str) -> Vec<std::path::PathBuf> {
     let mut files: Vec<std::path::PathBuf> = Vec::new();
-    for element in std::path::Path::new(TO_RESTORE_PATH)
+    for element in std::path::Path::new(&path)
         .read_dir()
-        .expect(&format!("Error on read files in {}", TO_RESTORE_PATH))
+        .expect(&format!("Error on read files in {}", &path))
     {
         let path = element.unwrap().path();
         if let Some(extension) = path.extension() {
-            if extension.to_ascii_uppercase() == "7Z" {
+            if extension.to_ascii_uppercase() == *ext {
                 files.push(path);
             }
         }
@@ -64,7 +90,19 @@ fn restore_database(backup_name: &str) {
 fn unzip(file: &str) {
     std::process::Command::new(Z_PATH)
         .args(&["x", &format!("-o{}", &TEMP_PATH), file])
-        // .spawn()
         .output()
-        .expect(&format!("Error on unzip file {}", file));
+        .expect(&format!("Error on unzip file {}", &file));
+}
+
+fn zip(name_zip: &str) {
+    std::process::Command::new(Z_PATH)
+        .args(&[
+            "a",
+            &format!("{}/{}", &TEMP_PATH, &name_zip),
+            &format!("{}/APPDATABASE.fdb", &TEMP_PATH),
+            &format!("{}/log.txt", &TEMP_PATH),
+            &format!("{}/error.txt", &TEMP_PATH),
+        ])
+        .output()
+        .expect(&format!("Error on zip file {}", &name_zip));
 }
